be6efd0e77fc94e58b5a922e63b8269853c9d2bb
thepwrtank18 <leoholdiman7@gmail.com>
2020-06-13 19:07:16 -0400

    added block

--- src/main/java/net/thetank/uncraft/UncraftingTable.java
+++ src/main/java/net/thetank/uncraft/UncraftingTable.java
@@ -1,8 +1,12 @@
 package net.thetank.uncraft;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 
-public class UncraftingTable implements ModInitializer {
+public class UncraftingTable implements ModInitializer 
+{
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -10,5 +14,9 @@ public class UncraftingTable implements ModInitializer {
 		// Proceed with mild caution.
 
 		System.out.println("Hello Fabric world!");
+
+		// an instance of the great table
+		public static final Block UNCRAFTING_TABLE = new Block(FabricBlockSettings.of(Material.STONE).build());
+		[...]
 	}
 }
