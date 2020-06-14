af14ddc22375dad9e417001e9f1db7e6f432c960
thepwrtank18 <leoholdiman7@gmail.com>
2020-06-14 03:00:28 -0400

    Update UncraftingTable.java

--- src/main/java/net/thetank/uncraft/UncraftingTable.java
+++ src/main/java/net/thetank/uncraft/UncraftingTable.java
@@ -1,8 +1,25 @@
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class UncraftingTable extends Block {
 
     public UncraftingTable(final Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(final BlockState state, final World world, final BlockPos pos, final PlayerEntity player,
+            final Hand hand, final BlockHitResult hit) {
+        if (!world.isClient) {
+        }
+ 
+        return ActionResult.SUCCESS;
     }
 }
